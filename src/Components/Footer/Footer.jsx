bb3c31e18a5aedd84c53930b13c17b1e20844b69
Lucas Mofardini <lucasmofardini@gmail.com>
2022-12-02 01:02:27 -0300

    Header e Footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style_footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container-footer">
+        <a
+          href="https://github.com/lucasmofardini"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <p>@lucasmofardini</p>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style_header.css";
+// import logo from "../../Assets/images/2022_FIFA_World_Cup.svg.png";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="nav-bar">
+         <h1>Copa do Mundo 2022</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
