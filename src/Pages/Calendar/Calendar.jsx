e1efcad727d9daa35f45fb2bda2a1c18657e6931
Lucas Mofardini <lucasmofardini@gmail.com>
2022-12-03 15:08:40 -0300

    Calendar Component create and first request

--- src/Pages/Calendar/Calendar.jsx
+++ src/Pages/Calendar/Calendar.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import ImageLogo from '../../Assets/images/2022_FIFA_World_Cup.svg.png';
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
-import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import "./style_calendar.css";
 
 // import required modules
-import { FreeMode, Pagination } from "swiper";
+import { FreeMode, Navigation } from "swiper";
 
 const Calendar = () => {
+  
+  const loadInfos = async () => {
+    const res = await fetch('https://copa22.medeiro.tech/matches');
+    const data = await res.json();
+    console.log(data)
+
+  } 
+  useEffect(() => {
+    loadInfos()
+  },[]);
+
   return (
     <>
+    <section className="section-calendar">
+      <div className="container-header-calendar">
+        <img className="img-logo-header-calendar" src={ImageLogo} alt="" /> <h1>CALENDÁRIO</h1>
+      </div>
      <Swiper
         slidesPerView={3}
         spaceBetween={30}
         freeMode={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[FreeMode, Pagination]}
+        rewind={true}
+        navigation={true}
+        modules={[Navigation, FreeMode]}
         className="mySwiper"
       >
         <SwiperSlide>Slide 1</SwiperSlide>
@@ -35,6 +49,7 @@ const Calendar = () => {
         <SwiperSlide>Slide 8</SwiperSlide>
         <SwiperSlide>Slide 9</SwiperSlide>
       </Swiper>
+      </section>
     </>
   );
 };
